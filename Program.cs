0f750cc1be40e6aef30aff4aab97800f007895fa
kolya5544 <05-kolya@bk.ru>
2020-06-29 18:21:14 +0300

    Started creating a new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiffieHellman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===[ Starting server at 7887 port ]===");
+        }
+    }
+}
